1de91d0d8d1b1dbc525815053bee7eade6f5a036
martuza <martuzakhadri12@gmail.com+>
2023-06-02 21:05:26 +0530

    password hash change

--- routes/users.js
+++ routes/users.js
@@ -124,7 +124,7 @@ router.post('/register', async (req,res)=>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        passwordHash:bcrypt.hashSync(req.body.passwordHash,10),
         phone: req.body.phone,
         isAdmin: req.body.isAdmin,
         street: req.body.street,
